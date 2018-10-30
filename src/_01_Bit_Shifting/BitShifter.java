082db92e1c717fa0814b364c2d5a7fd6b32046c7
CatcusCat <league@iMac-5.attlocal.net>
2018-10-29 19:01:47 -0700

    Finished Bit Shifter

--- src/_01_Bit_Shifting/BitShifter.java
+++ src/_01_Bit_Shifting/BitShifter.java
@@ -4,14 +4,12 @@ public class BitShifter {
 	public static void main(String[] args) {
 		// 1. Jot down the value of num in binary.
 		int num = 4;
-		int numShifted = num << 0;
-		
+		int numShifted = num << 1;
 		// 2. Print the value of numShifted, and convert that value to binary.
-		
-		// 3. Compare the two binary values. Can you figure out what the << operator is for?
-		
+		System.out.println(numShifted);
+		// 3. The << operator adds a zero for each number at the end of the number. 10 becomes 100, etc.
 		// 4. Try shifting num 3 places.
-		
+		System.out.println(num << 3);
 		// FYI: Binary values can be shifted to the right as well using the >> operator.	
 	}
 }
