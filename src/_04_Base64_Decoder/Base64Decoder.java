ed8ca02f406b279d814f476340f1890dba9881f7
SquawkyToucan <ewbrowne743@gmail.com>
2018-10-31 19:41:44 -0700

    Finished Module 4

--- src/_04_Base64_Decoder/Base64Decoder.java
+++ src/_04_Base64_Decoder/Base64Decoder.java
@@ -1,7 +1,7 @@
 package _04_Base64_Decoder;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+//import java.io.BufferedReader;
+//import java.io.FileReader;
 import java.util.Arrays;
 
 public class Base64Decoder {
@@ -42,12 +42,32 @@ public class Base64Decoder {
 	// characters long and return an array of 3 bytes (24 bits). The byte
 	// array should be the binary value of the encoded characters.
 	public static byte[] convert4CharsTo24Bits(String s) {
-		return null;
+		//"abcd" -> 24 bytes = 6 bits/char
+		byte[] bytes = new byte[3];
+		char[] chars = new char[4];
+		chars = s.toCharArray();
+		
+		bytes[0] = (byte) ((convertBase64Char(chars[0]) << 2) + (convertBase64Char(chars[1])>>4));
+		bytes[1] = (byte) ((convertBase64Char(chars[1]) << 4) + (convertBase64Char(chars[2])>>2));
+		bytes[2] = (byte) ((convertBase64Char(chars[2]) << 6) + (convertBase64Char(chars[3])));
+		
+		return bytes;
 	}
 
 	// 3. Complete this method so that it takes in a string of any length
 	// and returns the full byte array of the decoded base64 characters.
 	public static byte[] base64StringToByteArray(String file) {
-		return null;
+		// create the array of bytes
+		byte[] bytes = new byte[(file.length() / 4) * 3];
+		// fill the array of bytes
+		int miniArrayFiller = 0;
+		for (int i = 0; i < file.length(); i = i + 4) {
+			byte[] bts = convert4CharsTo24Bits(file.substring(i, i + 4));
+			for (int j = 0; j < 3; j++) {
+				bytes[miniArrayFiller + j] = bts[j];
+			}
+			miniArrayFiller+=3;
+		}
+		return bytes;
 	}
 }
